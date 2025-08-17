7352a83591a8c8b259ee5f4fb59dcd6cf37c0a93
Sultan Room <sultanroomcode@gmail.com>
2025-08-17 23:22:39 +0700

    install bcrypt and test it into signUp method

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,18 @@
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3, { message: 'minimal tiga karakter untuk username' })
+  @MaxLength(20, { message: 'maksimal 20 karakter untuk username' })
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3, { message: 'minimal tiga karakter' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password harus ada karakter besar, karakter huruf kecil, karakter angka dan karakter spesial',
+  })
   password: string;
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,8 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
@@ -10,8 +12,19 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
-
-    await this.save(user);
+    // generate salt
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // Username already exists
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException('who knows??');
+      }
+    }
   }
 }
